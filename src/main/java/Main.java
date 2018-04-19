f03fbcd19e50acec8340d9a500604dfd57dc0248
uda4nik <yuriy.yermolenko@gmail.com>
2018-04-19 09:06:09 +0200

    Added plugins to make fat jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 
-    public static final String MY_LINE_SEPARATOR = System.getProperty("line.separator");
-
     public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext config = new AnnotationConfigApplicationContext(SpringConfig.class);
         ServerBootstrap serverBootstrap = config.getBean(ServerBootstrap.class);
